9dd1b0c79cbb1f782318d0483dd07c24eaa8a2f1
Finn Lancaster <flancast90@gmail.com>
2021-06-19 15:21:45 -0400

    Add files via upload

---
+++ ParPy/example/example.py
@@ -0,0 +1,18 @@
+# ParPy: A python natural language
+# parser based on the one used
+# by Zork, for use in Python games
+
+# Copyright (c) Finn Lancaster 2021
+
+# This file contains the example usage
+# of the ParPy repo
+
+from ParPy import *
+
+def mainFunction():
+    inp = input("Enter a command (Ex. Hit James) or (Ex. Hit James with ...)\n")
+    print(ParPy(inp))   
+    mainFunction()
+
+mainFunction()
+
